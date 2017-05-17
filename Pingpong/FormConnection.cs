a42624239a288ef4f0fcb48db88a313d233f079d
eazy12 <eazy12kzn@gmail.com>
2017-05-17 21:19:27 +0300

    Добавлено меню с выбором адреса и никнейма.

---
+++ Pingpong/FormConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Pingpong
+{
+    public partial class FormConnection : Form
+    {
+        private static string Address = "localhost";
+        public FormConnection()
+        {
+            InitializeComponent();
+            textBox1.Text = "localhost";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Address = textBox1.Text;
+        }
+
+        public static string GetAddress()
+        {
+            return Address;
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = System.Drawing.Color.Black;
+        }
+    }
+}
